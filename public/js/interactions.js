138bce989a40b966965971dc689cd79255630a06
nilto <niltorrents99@gmail.com>
2026-08-02 00:10:06 +0200

    Forzar update cache

--- public/js/interactions.js
+++ public/js/interactions.js
@@ -1,5 +1,5 @@
-import { showToast } from './utils/toast.js?v=202608011250';
-import { buildTreatmentModalContent } from './components/services.js?v=202608011250';
+import { showToast } from './utils/toast.js?v=202608020010';
+import { buildTreatmentModalContent } from './components/services.js?v=202608020010';
 
 // --- Navegación Móvil ---
 function initializeMobileNavigation() {
